328c50d1315412f6dde7721742b447b04fb60a69
Lorderot <Ihorkayf_@mail.ru>
2017-05-24 02:09:29 +0300

    Added CSharp examples

---
+++ CSharp/MatchExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace RegExp
+{
+    public class MatchExample
+    {
+        public static void Run()
+        {
+            const string input = "Hello user1@domain.com and user2@domain.com";
+            
+            var regex = new Regex("\\S+@[\\S.]+");
+            var results = regex.Matches(input)
+                               .OfType<Match>();
+
+            Console.WriteLine("Match Example:");
+            Console.WriteLine("Original string: {0} \n" +
+                              "Mathces: {1}",
+                              input,
+                              string.Join(", ", results.Select(r => r.Value)));
+        }
+    }
+}
---
+++ CSharp/ReplaceExample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegExp
+{
+    public class ReplaceExample
+    {
+        public static void Run()
+        {
+            var input = "abcdef";
+            var regex = new Regex("abc");
+
+            var result = regex.Replace(input, "123");
+
+            Console.WriteLine("Original string {0}, " +
+                              "Replacement string: {1}", 
+                              input, 
+                              result);
+
+            input = "abcdefABCEF";
+            regex = new Regex("abc", RegexOptions.IgnoreCase);
+
+            result = regex.Replace(input, "123");
+
+            Console.WriteLine("Ignore case:");
+            Console.WriteLine("Original string {0}, " +
+                              "Replacement string: {1}",
+                              input,
+                              result);
+        }
+
+    }
+}
